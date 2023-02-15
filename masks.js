4b48c63cdba40e9431c27bc42e95604accdc0771
Mario <mariohcezar@gmail.com>
2023-02-15 20:22:44 -0300

    Update masks.js

--- masks.js
+++ masks.js
@@ -4,16 +4,16 @@ input.addEventListener("keyup", formatarMoeda);
 
 function formatarMoeda(e) {
 
-let valor = e.target.value.replace(/\D/g,"");
+let v = e.target.value.replace(/\D/g,"");
 
-valor = (valor/100).toFixed(2) + "";
+v = (v/100).toFixed(2) + "";
 
-valor = valor.replace(".", ",");
+v = v.replace(".", ",");
 
-valor = valor.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
 
-valor = valor.replace(/(\d)(\d{3}),/g, "$1.$2,");
+v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
 
-e.target.value = valor;
+e.target.value = v;
 
 }
