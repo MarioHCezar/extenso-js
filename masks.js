d4f1196bcecf5c3482868763246fb4d857f4b2e9
Mario <mariohcezar@gmail.com>
2023-02-15 20:04:53 -0300

    Update masks.js

--- masks.js
+++ masks.js
@@ -1,20 +1,3 @@
-const cpf = document.querySelector(".cpf");
-
-input.addEventListener("keyup", formatarCPF);
-
-function formatarCPF(e){
-
-let valor =e.target.value.replace(/\D/g,"");
-
-valor=valor.replace(/(\d{3})(\d)/,"$1.$2");
-
-valor=valor.replace(/(\d{3})(\d)/,"$1.$2");
-
-valor=valor.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
-
-e.target.value = valor;
-}
-
 const valor = document.querySelector("#valor");
 
 input.addEventListener("keyup", formatarMoeda);
