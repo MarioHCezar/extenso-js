f5266158810d4d707129c2b283dce5645489e09a
Mario <mariohcezar@gmail.com>
2023-02-15 09:43:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const form = document.querySelector("form");
 const botao = document.querySelector(".botao");
+const pf = document.querySelector("#checkCPF");
+const cnpj = document.querySelector("#checkCNPJ");
+const select = document.querySelector("select");
 
 let nome_dec = document.querySelector(".nome");
 let endereco_dec = document.querySelector(".endereco");
@@ -12,10 +15,7 @@ let extenso_dec = document.querySelector(".extenso");
 let local_dec = document.querySelector(".local");
 let data_dec = document.querySelector(".data");
 
-
-botao.addEventListener("click", (e) => {
-  e.preventDefault();
-
+botao.addEventListener("click", () => {
   nome_dec.innerText = form.nome.value;
   endereco_dec.innerText = form.endereco.value;
   documento_dec.innerText = form.documento.value;
@@ -25,9 +25,13 @@ botao.addEventListener("click", (e) => {
   valor_dec.innerText = form.valor.value;
   let extenso = form.valor.value;
   extenso_dec.innerText = extenso.extenso(",");
-  console.log(extenso_dec)
+  if (select.options[select.selectedIndex].value === "cpf") {
+    pf.checked = true;
+  } else {
+    cnpj.checked = true;
+  }
   local_dec.innerText = form.local.value;
   let data = new Date(form.data.value);
-  data_dec.innerText = data.toLocaleDateString("pt-BR", { timeZone: "UTC" });;
+  data_dec.innerText = data.toLocaleDateString("pt-BR", { timeZone: "UTC" });
   print();
 });
