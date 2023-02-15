90772e96cb78c8ba4e70c6aeb6bac1f34b2a49ed
Mario <mariohcezar@gmail.com>
2023-02-15 20:11:29 -0300

    Update masks.js
    "Corrigindo máscara de valor"

--- masks.js
+++ masks.js
@@ -1,17 +1,19 @@
-const valor = document.querySelector("#valor");
+const input = document.querySelector("#valor");
 
-input.addEventListener("keyup", formatarMoeda);
+input.addEventListener("keyup", formatarMoeda); 
 
 function formatarMoeda(e) {
-  let valor = e.target.value.replace(/\D/g, "");
 
-  valor = (valor / 100).toFixed(2) + "";
+let valor = e.target.value.replace(/\D/g,"");
 
-  valor = valor.replace(".", ",");
+valor = (valor/100).toFixed(2) + "";
 
-  valor = valor.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+valor = valor.replace(".", ",");
 
-  valor = valor.replace(/(\d)(\d{3}),/g, "$1.$2,");
+valor = valor.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
 
-  e.target.value = valor;
+valor = valor.replace(/(\d)(\d{3}),/g, "$1.$2,");
+
+e.target.value = valor;
+
 }
