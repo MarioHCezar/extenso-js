1047c31258e0649ee26e1e1f38d004014a09b40f
Mario <mariohcezar@gmail.com>
2023-02-16 01:51:25 -0300

    "Adjustments"

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const form = document.querySelector("form");
 const botao = document.querySelector(".botao");
 const pf = document.querySelector("#checkCPF");
-const cnpj = document.querySelector("#checkCNPJ");
+const pj = document.querySelector("#checkCNPJ");
 const select = document.querySelector("select");
-//const cpf = document.querySelector(".cpf");
 
 let nome_dec = document.querySelector(".nome");
 let endereco_dec = document.querySelector(".endereco");
@@ -15,6 +14,31 @@ let valor_dec = document.querySelector(".valor");
 let extenso_dec = document.querySelector(".extenso");
 let local_dec = document.querySelector(".local");
 let data_dec = document.querySelector(".data");
+botao.classList.add("botao__disabled");
+
+function checkInputs(inputs) {
+  let filled = true;
+  inputs.forEach(function (input) {
+    if (input.value === "") {
+      filled = false;
+    }
+  });
+  return filled;
+
+}
+
+let inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("change", function () {
+    if (checkInputs(inputs)) {
+      botao.disabled = false;
+      botao.classList.remove("botao__disabled");
+    } else {
+      botao.disabled = true;
+    }
+  });
+});
 
 botao.addEventListener("click", () => {
   nome_dec.innerText = form.nome.value;
@@ -22,18 +46,22 @@ botao.addEventListener("click", () => {
   documento_dec.innerText = form.documento.value;
   processo_dec.innerText = form.processo.value;
   vara_dec.innerText = form.vara.value;
-  secao_dec.innerText = form.secao.value;
+  secao_dec.innerText = `${form.secao.value}, `;
   valor_dec.innerText = form.valor.value;
   let extenso = form.valor.value;
   extenso_dec.innerText = extenso.extenso(",");
   if (select.options[select.selectedIndex].value === "cpf") {
     pf.checked = true;
   } else {
-    cnpj.checked = true;
+    pj.checked = true;
     cpf.maxLength = 18;
   }
-  local_dec.innerText = form.local.value;
+  local_dec.innerText = `${form.local.value}, `;
   let data = new Date(form.data.value);
-  data_dec.innerText = data.toLocaleDateString("pt-BR", { timeZone: "UTC" });
+  data_dec.innerText = `${data.toLocaleDateString("pt-BR", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  })}.`;
   print();
 });
