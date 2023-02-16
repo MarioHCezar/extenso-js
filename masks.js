b8fe62154ae8fe41f08eb001d9c89a6d76f661b8
Mario <mariohcezar@gmail.com>
2023-02-16 09:52:25 -0300

    Update masks.js

--- masks.js
+++ masks.js
@@ -37,3 +37,22 @@ v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
 e.target.value = v;
 
 } 
+
+//cnpj
+const input = document.querySelector(".documento");
+input.addEventListener("keyup", formatarCNPJ);
+
+function formatarCNPJ(e){
+
+var v= e.target.value.replace(/\D/g,"");
+
+v=v.replace(/^(\d{2})(\d)/,"$1.$2");
+
+v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3");
+
+v=v.replace(/\.(\d{3})(\d)/,".$1/$2");
+
+v=v.replace(/(\d{4})(\d)/,"$1-$2");  
+
+e.target.value = v;
+}
